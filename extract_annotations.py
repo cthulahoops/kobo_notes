9909e4dd62967dec83b6eeccedb77699f01ee8c5
Adam Kelly <adam@cthulahoops.org>
2023-11-17 10:09:30 +0000

    Use the html2text library to convert the book description to markdown.

--- extract_annotations.py
+++ extract_annotations.py
@@ -4,7 +4,8 @@ import itertools
 from pathlib import Path
 import click
 import sqlite3
-from jinja2 import Environment, FileSystemLoader, select_autoescape
+import html2text
+from jinja2 import Environment, FileSystemLoader
 
 ANNOTATIONS = """
     select
@@ -37,6 +38,39 @@ BOOKS = """
 """
 
 
+@click.command()
+@click.argument("db_file", type=click.Path(exists=True))
+@click.option("-o", "--output-dir", type=click.Path(exists=True, file_okay=False))
+def main(db_file, output_dir):
+    with sqlite3.connect(db_file) as conn:
+        conn.row_factory = sqlite3.Row
+        books = {book["ContentID"]: book for book in query(conn, BOOKS)}
+        annotations = query(conn, ANNOTATIONS)
+
+    env = Environment(
+        loader=FileSystemLoader("."),
+        autoescape=True,
+    )
+    env.filters["html_to_markdown"] = html_to_markdown
+    template = env.get_template("book.md.j2")
+
+    for content_id, annotations in group_by_key(annotations, "VolumeID"):
+        book = books[content_id]
+
+        annotated_days = itertools.groupby(annotations, annotation_date)
+
+        output = template.render(
+            book=book,
+            annotated_days=annotated_days,
+        )
+        with open(output_filename(output_dir, book), "w") as output_file:
+            output_file.write(output)
+
+
+def html_to_markdown(html):
+    return html2text.HTML2Text().handle(html)
+
+
 def annotation_date(annotation):
     return datetime.strptime(annotation["DateCreated"], "%Y-%m-%dT%H:%M:%S.%f").date()
 
@@ -54,35 +88,8 @@ def query(conn, query):
     return cur.fetchall()
 
 
-def output_path(book):
-    return Path("output") / f"{book['Title']}.md"
-
-
-@click.command()
-@click.argument("db_file", type=click.Path(exists=True))
-def main(db_file):
-    with sqlite3.connect(db_file) as conn:
-        conn.row_factory = sqlite3.Row
-        books = {book["ContentID"]: book for book in query(conn, BOOKS)}
-        annotations = query(conn, ANNOTATIONS)
-
-    env = Environment(
-        loader=FileSystemLoader("."),
-        autoescape=select_autoescape(["html", "md"]),
-    )
-    template = env.get_template("book.md.j2")
-
-    for content_id, annotations in group_by_key(annotations, "VolumeID"):
-        book = books[content_id]
-
-        annotated_days = itertools.groupby(annotations, annotation_date)
-
-        output = template.render(
-            book=book,
-            annotated_days=annotated_days,
-        )
-        with open(output_path(book), "w") as output_file:
-            output_file.write(output)
+def output_filename(output_dir, book):
+    return Path(output_dir) / f"{book['Title']}.md"
 
 
 if __name__ == "__main__":
